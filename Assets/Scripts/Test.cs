9ba8e00c3b624cbcbe4bb980c109ed931f8dd4b5
Kshitij Kumar <whkshitij13@gmail.com>
2024-01-22 00:10:09 +0530

    Movement and Rigidbody

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
- 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Debug.Log("entered a gameobject"+collision.gameObject.name);
-    //}
-    //private void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    Debug.Log("exit a gameobject" + collision.gameObject.name);
-    //}
-    private void OnTriggerEnter2D(Collider2D collision)
+    public Rigidbody2D rb;
+    public Test2 t2;
+    private void Start()
     {
-        Debug.Log("enter a gameobject" + collision.gameObject.name);
+        rb = GetComponent<Rigidbody2D>();
+        t2=GetComponent<Test2>();
+
     }
-    private void OnTriggerExit2D(Collider2D collision)
+
+  
+    private void Update()
     {
-        Debug.Log("exit a gameobject" + collision.gameObject.name);
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.position =new Vector3(transform.position.x-0.04f, transform.position.y, transform.position.z);   //changing position through transform
+            //transform.Translate(new Vector3(transform.position.x - 0.04f, transform.position.y, transform.position.z));
+            //rb.AddForce(Vector3.left);
+            //rb.velocity = Vector3.left; 
+            //rb.MovePosition(Vector3.left);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x + 0.04f, transform.position.y, transform.position.z);
+        }
     }
 }
 
